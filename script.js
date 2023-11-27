acdbc6bad86fe9f9ef8f929ed4acf082659c7db4
AdriaMelero1 <adriamelero1@gmail.com>
2023-11-27 21:17:41 +0100

    Letras cambian de color

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
+//Capture elements from DOM
+const container = document.querySelector('.container');
+const availableWords = document.querySelectorAll('.row .letter:not(.contained) .letter:not(.not-contained)');
+const secretWord = document.getElementById('secretWord');
+
+
+
 const word = "Meleeerro";
-const arr = [...word]
+const arr = [...word.toLocaleLowerCase()];
+
+let bars = [];
+
+createUnderscores();
 
 
 
 
-const secretWord = document.getElementById('secretWord')
-let bars = '';
+function createUnderscores() {
 
-arr.forEach((input) => {
-	bars+= '_';
+	arr.forEach((input) => {
+		bars += '_';
+	});
+
+	secretWord.innerText = bars;
+}
+
+
+container.addEventListener('click', (e) => {
+	if (e.target.classList.contains('letter')
+		&& !e.target.classList.contains('contained')
+		&& !e.target.classList.contains('not-contained')) {
+
+		checkIfIsContained(e);
+	}
 });
 
-secretWord.innerText = bars;
+
+
+
+function checkIfIsContained(letter){
+	if(arr.includes(letter.target.id)){
+		letter.target.classList.toggle('contained');
+	} else {
+		letter.target.classList.toggle('not-contained');
+	}
+	for(let i = 0; i < arr.length; i++){
+		if(arr[i] == letter.target.id){
+			bars[i] = letter.target.id;
+			
+			console.log(bars);
+			console.log(letter.target.id);
+		}
+		console.log(bars);
+		secretWord.innerText = bars;
+	}
+}
